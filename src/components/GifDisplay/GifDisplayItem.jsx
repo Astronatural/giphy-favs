fd0c15bc444a6aad2c11a335ea8e7cde184f85f0
Tre Olson <treolson@live.com>
2021-02-11 16:28:05 -0600

    added fav button

---
+++ src/components/GifDisplay/GifDisplayItem.jsx
@@ -0,0 +1,22 @@
+import { useSelector } from 'react-redux';
+
+
+function GifDisplayItem({gif}) {
+
+    const favoriteGif = () => {
+        console.log('Favoriting Gif..');
+    }
+
+    return (
+        <div>
+            <img src={gif.images.downsized_medium.url} />
+            <br />
+            <button onClick={() => {
+                favoriteGif()
+            }}>Favorite</button>
+        </div>
+    )
+
+}
+
+export default GifDisplayItem;
