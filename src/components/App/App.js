27f4fa6ea8bfa5bc87e76094b89d188f60f35e19
JDK <jdkranzler@yahoo.com>
2021-02-11 12:15:34 -0600

    still needs some work on the index side

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import SearchForm from '../SearchForm/SearchForm.jsx';  //  src/components/SearchForm/SearchForm.jsx
 
 function App(props) {
   return (
     <div>
       <h1>Giphy Search!</h1>
+      <SearchForm/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put } from 'redux-saga/effects';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function* searchGif(action) {
+    try {
+        // action.payload: { searchQ }
+        yield axios.post('/books', action.payload);
+        yield put({ type: 'FETCH_GIFS' });
+    } catch (error) {
+        console.log(`Error fetching books`, error);
+    }
+}
+
+// Create sagaMiddleware
+const sagaMiddleware = createSagaMiddleware();
+
+function* rootSaga() {
+    yield takeEvery('POST_SEARCH', searchGif);
+}
+
+// Pass rootSaga into our sagaMiddleware
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(<Provider>
+    <App />
+  </Provider> 
+    , document.getElementById('root'));
