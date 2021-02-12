00eb49da2a61576d31f3519d3ef61a3573ff878f
Tre Olson <treolson@live.com>
2021-02-12 13:29:23 -0600

    favorite funcitonality

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,42 +1,20 @@
-import {useEffect} from 'react';
 import { useSelector } from 'react-redux';
 import FavDisplayItem from './FavDisplayItem';
-import axios from 'axios';
 
 // change searchedGifs to favGifs
 
 function Favorites() {
 
-    const getFavs = () => {
-        console.log('Sending GET request..');
-    axios.get(`/api/favorite`)
-        .then((response) => {
-            console.log(response);
-            dispatch({
-                type: 'SET_FAVS',
-                payload: response
-            })
-            const favGifs = useSelector(store => store.favSearch);
-            console.log(favGifs);
+    const favGifs = useSelector(store => store.favSearch);
+    console.log(favGifs);
 
-            return (
-                <div>
-                    {favGifs.data.map(gif =>
-                        (<FavDisplayItem key={gif.id} gif={gif} />)
-                    )}
-                </div>
-            )
-        })
-        .catch(function (error) {
-            console.log(error);
-        }); 
-    }
-
-    useEffect(() => {
-        getFavs;
-    }, []);
-
-    
+    return (
+        <div>
+            {favGifs.data.map(gif =>
+                (<FavDisplayItem key={gif.id} gif={gif} />)
+            )}
+        </div>
+    )
 
 }
 
--- src/components/GifDisplay/GifDisplay.jsx
+++ src/components/GifDisplay/GifDisplay.jsx
@@ -1,9 +1,32 @@
-import { useSelector } from 'react-redux';
 import GifDisplayItem from './GifDisplayItem';
 import SearchForm from '../SearchForm/SearchForm';
+import { useEffect } from 'react';
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
 
 
 function GifDisplay() {
+
+    const dispatch = useDispatch();
+
+    const getFavs = () => {
+        console.log('Sending GET request..');
+        axios.get(`/api/favorite`)
+            .then((response) => {
+                console.log(response);
+                dispatch({
+                    type: 'SET_FAVS',
+                    payload: response
+                })
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    useEffect(() => {
+        getFavs();
+    }, []);
 
     const searchedGifs = useSelector(store => store.getSearch);
 
